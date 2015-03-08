5ae5463dcae567a9dbf2e591a4189c3db116cdf6
Romeo Van Snick <romeovs@gmail.com>
2015-03-08 23:08:46 +0100

    move browserify location

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 module.exports =
 { alert: require('./lib/alert')
 , dest:  require('./lib/dest')
-, react: { browserify: require('./lib/react/browserify.js') }
+, browserify: require('./lib/browserify')
 };
