7143104dad5e98af1130bd1edb48ffddae94e10c
Romeo Van Snick <romeovs@gmail.com>
2015-03-09 12:19:24 +0100

    add polyfills

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-"use strict";
+'use strict';
+
+require('babel/polyfill');
 
 module.exports = {
   alert      : require('./build/alert')
-, dest       :  require('./build/dest')
+, dest       : require('./build/dest')
 , browserify : require('./build/browserify')
 };
